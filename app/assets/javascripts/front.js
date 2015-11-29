4369678f6ba637ebbc64850976ac52e876b7fb99
lisiur <lisiur@163.com>
2015-11-29 10:54:16 +0800

    asdf

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -37,25 +37,49 @@ $(document).ready(function(){
       $(this).find("ul.subnav:first").stop(true).slideUp("fast");
 
   });
-
-// 监听滚动事件
-  var dis = $("#nav-menu").offset().top;
-  var marginTop = $(".main").css("margin-top");
+  $('.logo').click(function(){
+    if(!$('#navs').is(":visible")){
+      $('#navs').animate({
+        left:-$('#navs').width(),
+      },0,function(){
+        $('#navs').show("fast");
+        $('#navs').animate({
+          left:"0",
+        },100)
+      })
+    }else{
+      $('#navs').animate({
+        left:-$('#navs').width(),
+      },100,function(){
+        $('#navs').hide("fast");
+      })
+    }
+  })
+//监听滚动事件
+  // var dis = $("#nav-menu").offset().top;
+  // var marginTop = $(".main").css("margin-top");
+  // $(window).scroll(function(){
+	// 	var height = $(window).scrollTop();
+	// 	if(height>=dis){
+	// 		$("#nav-menu").addClass("navbar-fixed-top");
+	// 		$("#nav-menu").css("margin-top",0);//取消菜单的margin-top使其完全贴近顶部
+  //     $(".main").css("margin-top","85px");
+  //     //监听右下方快捷上滚
+  //
+	// 	}else{
+	// 		$("#nav-menu").removeClass("navbar-fixed-top");
+  //     $(".main").css("margin-top",marginTop);
+	// 	}
+  // });
   $(window).scroll(function(){
-		var height = $(window).scrollTop();
-		if(height>=dis){
-			$("#nav-menu").addClass("navbar-fixed-top");
-			$("#nav-menu").css("margin-top",0);//取消菜单的margin-top使其完全贴近顶部
-      $(".main").css("margin-top","85px");
-      //监听右下方快捷上滚
+    var height = $(window).scrollTop();
+    var dis = $(".main").offset().top;
+    if(height>=dis){
       $(".upupup").show("slow");
-		}else{
-			$("#nav-menu").removeClass("navbar-fixed-top");
-      $(".main").css("margin-top",marginTop);
+    }else{
       $(".upupup").hide("slow");
-		}
-  });
-
+    }
+  })
   $('.upupup').click(function(){
     $("html,body").animate({scrollTop:"0"},1000)
   })
